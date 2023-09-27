21558775ea1a4a1586b7ebfee7b616f9dcc27168
Cedric Joel Ngameni Pokam <molarbobar@yahoo.fr>
2023-09-27 21:17:07 +0200

    added dataprovider-base

---
+++ src/dataprovider.ts
@@ -0,0 +1,106 @@
+import { fetchUtils } from 'react-admin';
+import { stringify } from 'query-string';
+
+const apiUrl = 'https://my.api.com/';
+const httpClient = fetchUtils.fetchJson;
+
+export default {
+    getList: async (resource: String, params: any) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        const { json, headers } = await httpClient(url);
+        return {
+            data: json,
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        };
+    },
+
+    getOne: async (resource: String, params: any) => {
+        const url = `${apiUrl}/${resource}/${params.id}`
+        const { json } = await httpClient(url);
+        return { data: json };
+    },
+
+    getMany: async (resource: String, params: any) => {
+        const query = {
+            filter: JSON.stringify({ ids: params.ids }),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        const { json } = await httpClient(url);
+        return { data: json };
+    },
+
+    getManyReference: async (resource: String, params: any) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify({
+                ...params.filter,
+                [params.target]: params.id,
+            }),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        const { json, headers } = await httpClient(url);
+        return {
+            data: json,
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        };
+    },
+
+    create: async (resource: String, params: any) => {
+        const { json } = await httpClient(`${apiUrl}/${resource}`, {
+            method: 'POST',
+            body: JSON.stringify(params.data),
+        })
+        return { data: json };
+    },
+
+    update: async (resource: String, params: any) => {
+        const url = `${apiUrl}/${resource}/${params.id}`;
+        const { json } = await httpClient(url, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        })
+        return { data: json };
+    },
+
+    updateMany: async (resource: String, params: any) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids}),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        const { json } = await httpClient(url, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        })
+        return { data: json };
+    },
+
+    delete: async (resource: String, params: any) => {
+        const url = `${apiUrl}/${resource}/${params.id}`;
+        const { json } = await httpClient(url, {
+            method: 'DELETE',
+        });
+        return { data: json };
+    },
+
+    deleteMany: async (resource: String, params: any) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids}),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        const { json } = await httpClient(url, {
+            method: 'DELETE',
+            body: JSON.stringify(params.data),
+        });
+        return { data: json };
+    },
+};
