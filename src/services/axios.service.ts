ef58c544ebe2e4eaedaf60334fbf1fd8e2a53381
Cedric Joel Ngameni Pokam <molarbobar@yahoo.fr>
2023-11-06 10:09:29 +0100

    Create first done

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -10,7 +10,7 @@ export default (resource: string) => ({
 
   update: (path: string, params: any) => axios.put(`${resource}/${path}/${params.id}`, params.data),
 
-  create: (path: string, params: any) => axios.post(`${resource}/${path}/`, params.data),
+  create: (path: string, params: any) => axios.post(`${resource}/${path}/`, params),
 
   delete: (path: string, id: string | number) => axios.delete(`${resource}/${path}/${id}`)
 })
